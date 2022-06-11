5c5d8d801d78431649e47a547e83018be18f3586
User4 <titvjl9009@mail.ru>
2022-06-11 10:36:18 +0200

    Добавьте файлы проекта.

---
+++ Lab 1/Class5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    internal class Class5
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Class5(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Result()
+        {
+            return A * B;
+        }
+    }
+}
---
+++ Lab 1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Class5 class5 = new Class5(double.Parse(x1.Text), double.Parse(x2.Text));
+            end.Text = class5.Result().ToString();
+        }
+    }
+}
